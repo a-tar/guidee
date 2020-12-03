8694a8a12c17b90f7d3c7bcb4447983a91f4c1c7
a-tar <a-tar>
2020-12-04 03:34:32 +0700

    Project

---
+++ app/src/main/java/com/anjas/kotlinapp/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.anjas.kotlinapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.ProcessBuilder.Redirect.to
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var button:Button
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        button =findViewById(R.id.btn_explore)
+        button.setOnClickListener {
+            startActivity (Intent(this@MainActivity, HomeActivity::class.java))
+            finish()
+        }
+
+    }
+}
